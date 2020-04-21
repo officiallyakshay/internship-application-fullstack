1d429b281ddcc773e0bec64ac0c50087303181bf
casualshay <akshaysprabhakar@gmail.com>
2020-04-21 13:34:31 -0700

    Extra Credit part 1 done

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
+class ElementHandler {
+  element(element) {
+    if (element.tagName === 'title') {
+      element.setInnerContent('Akshay Prabhakar ');
+    }
+    if (element.tagName === 'h1' && element.getAttribute('id') === 'title') {
+      element.prepend('Akshay Prabhakar ');
+    }
+    if (element.tagName === 'p') {
+      element.setInnerContent('Learn more about me through my Linkedin!');
+    }
+    if (element.tagName === 'a') {
+      element.setAttribute('href', 'https://www.linkedin.com/in/akshay-prabhakar/');
+      element.setInnerContent('My Linkedin')
+    }
+  }
+}
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
   // return event.response;
-})
+});
 
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  // return new Response('Hello worker!', {
-  //   headers: { 'content-type': 'text/plain' },
-  // })
   try {
     let links = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
     let data = await links.json();
     let rand = Math.floor( Math.random() * 2 )
-    let response = data.variants[rand];
-    console.log(response);
-    return new Response(response);
+    
+    let request = new Request(data.variants[rand])
+    let result = await fetch(request)
+    result = new Response(result.body, result)
+    
+    var htmlwriter = new HTMLRewriter()
+    htmlwriter
+      .on('title', new ElementHandler())
+      .on('h1', new ElementHandler())
+      .on('p', new ElementHandler())
+      .on('a', new ElementHandler())
+    return htmlwriter.transform(result)
+    // return new Response(response);
   }
   catch(ex) {
     console.error('Error:', ex);
